44f26580bb9231636a4933973cf501edc7711360
Ben Chang <blchang@ucdavis.edu>
2020-04-02 16:54:37 -0700

    changes to pokemon go

--- pokemongoanalysis.py
+++ pokemongoanalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 # Read in data
 df = pd.read_csv('C:/Users/bench/Documents/Python Scripts/Scripts/Pokemon/pokemongo.csv', index_col=0, encoding= 'unicode_escape')
@@ -10,15 +11,19 @@ df["Total"]=df['Att']+df['Def']+df['Sta']
 df.sort_values("Total",ascending=False)
 
 plt.figure(0,figsize=(15,5))
-sns.lmplot(x='Att', y='Max CP', data=df)
+sns.lmplot(x='Att', y='MaxCP', data=df)
 
 plt.figure(1,figsize=(15,5))
-sns.lmplot(x='Def', y='Max CP', data=df)
+sns.lmplot(x='Def', y='MaxCP', data=df)
 
 plt.figure(2,figsize=(15,5))
-sns.lmplot(x='Sta', y='Max CP', data=df)
+sns.lmplot(x='Sta', y='MaxCP', data=df)
 
 plt.figure(3,figsize=(15,5))
-sns.lmplot(x='Total', y='Max CP', data=df)
-plt.tight_layout()
-plt.show()
+sns.lmplot(x='Total', y='MaxCP', data=df)
+
+sns.distplot(df.MaxCP)
+
+# model = LinearRegression()
+# X, y = df[['NumberofEmployees','ValueofContract']], df.AverageNumberofTickets
+# model.fit(X, y)

816724856cc3f24125583c54d664aff9c274a710
Ben Chang <blchang@ucdavis.edu>
2020-04-06 21:11:01 -0700

    changes to analysis

--- pokemon.py
+++ pokemon.py
@@ -4,14 +4,23 @@ from matplotlib import pyplot as plt
 
 # Read in data
 df = pd.read_csv('C:/Users/bench/Documents/Python Scripts/Scripts/Pokemon/pokemoncomplete.csv', index_col=0, encoding= 'unicode_escape')
+df2 = pd.read_csv('C:/Users/bench/Documents/Python Scripts/Scripts/Pokemon/pokemongo.csv', index_col=0, encoding= 'unicode_escape')
 
-total_stats=df.sort_values("Total",ascending=False)
+df2["Total"]=df2['Att']+df2['Def']+df2['Sta']
+df2.describe()
 
-df.to_numpy
+df.sort_values(by="Total",ascending=False)
+df['totatt']=df['Attack']+df['Sp. Atk']
+df['totdef']=df['Defense']+df['Sp. Def']
 df.describe()
-df.sort_values(by="Speed",ascending=False)
 
-# df[df['Total'] > 599]
+pokemongolist=df2["Name"].tolist()
+pokemonlist=[]
+
+for pokemon in df['Name']:
+    if pokemon.upper()==df2.loc[:,'Name']:
+        pokemonlist=df['Name']
+
 
 pkmn_type_colors = ['#78C850',  # Grass
                     '#F08030',  # Fire
@@ -32,21 +41,13 @@ pkmn_type_colors = ['#78C850',  # Grass
 
 # Set theme
 sns.set_style('whitegrid')
-plt.figure(0,figsize=(10,6))
+plt.figure(figsize=(15,6))
 # Pre-format DataFrame
 stats_df = df.drop(['Legendary', 'Generation'], axis=1)
 # New boxplot using stats_df
 sns.boxplot(data=stats_df, palette=pkmn_type_colors)
 
-# Violin plot with Pokemon color palette
-plt.figure(1,figsize=(10,6))
-sns.violinplot(x='Type 1', y='Total', data=df,palette=pkmn_type_colors) # Set color palette
-
-# Set figure size with matplotlib
-plt.figure(2,figsize=(10,6))
-sns.swarmplot(x='Type 1', y='Total', data=df,palette=pkmn_type_colors)
-
-plt.figure(3,figsize=(10,6))
+plt.figure(figsize=(15,6))
 # Create plot
 sns.violinplot(x='Type 1',
                y='Total', 
--- pokemongoanalysis.py
+++ pokemongoanalysis.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import numpy as np
 import scipy
 from matplotlib import pyplot as plt
+from sklearn import metrics
 
 # Read in data
 df = pd.read_csv('C:/Users/bench/Documents/Python Scripts/Scripts/Pokemon/pokemongo.csv', index_col=0, encoding= 'unicode_escape')
@@ -14,17 +15,19 @@ df.sort_values("Total",ascending=False)
 calclist=['Att','Def','Sta','Total']
 
 for stat in calclist:
-    slope_stat, intercept_stat, rvalue_stat, pvalue_stat, std_stat = scipy.stats.linregress(df['MaxCP'],df[stat])
+    slope_stat, intercept_stat, rvalue_stat, pvalue_stat, std_stat = scipy.stats.linregress(df[stat],df['MaxCP'])
     r_squared_stat=rvalue_stat**2
+    ax=sns.lmplot(x=stat, y='MaxCP', data=df)
+    ax.fig.suptitle(stat+" vs Max CP")
+    y_pred=slope_stat*df[stat]+intercept_stat
     
     print("Statistical Values for "+stat)
     print("Slope: "+str(round(slope_stat,2)))
     print("Intercept: "+str(round(intercept_stat,2)))
-    print("Error: "+str(round(std_stat,4)))
-    print("R^2: "+str(round(r_squared_stat,2))+'\n')
-
-    ax=sns.lmplot(x=stat, y='MaxCP', data=df)
-    ax.fig.suptitle(stat+" vs Max CP")
+    print("R^2: "+str(round(r_squared_stat,2)))
+    print('Mean Absolute Error:', round(metrics.mean_absolute_error(df['MaxCP'], y_pred),2)) 
+    print('Root Mean Squared Error:', round(np.sqrt(metrics.mean_squared_error(df['MaxCP'], y_pred)),2))
+    print('\n')
 
 plt.figure(figsize=(15,5))
 sns.distplot(df.MaxCP).set_title('Distribution of Max CP')
@@ -32,9 +35,7 @@ sns.distplot(df.MaxCP).set_title('Distribution of Max CP')
 plt.figure(figsize=(15,5))
 sns.distplot(df.Total).set_title('Distribution of Total')
 
-
-
-
 plt.figure(figsize=(15,5))
 sns.boxplot(df['Type1'],df['MaxCP'],data=df)
-
+plt.figure(figsize=(15,5))
+sns.boxplot(df['Type2'],df['MaxCP'],data=df)

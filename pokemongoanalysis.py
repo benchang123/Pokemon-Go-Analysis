e4ff7e3303536f85a11a7a6307abc91780869627
Ben Chang <blchang@ucdavis.edu>
2020-08-28 14:56:22 -0700

    More Analysis added. Used new dataset for POGO

--- pokemongoanalysis.py
+++ pokemongoanalysis.py
@@ -1,27 +1,29 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np
-import scipy
 from matplotlib import pyplot as plt
 import os
-from sklearn import metrics
-from sklearn import linear_model
+from sklearn import linear_model as m
 
 #set working path
-default_path='C:/Users/bench/Documents/Python Scripts/Scripts/Pokemon'
+default_path='/Users/bench/Documents/Python Scripts/Scripts/Pokemon'
 os.chdir(default_path)
 
 
 # Read in data
+#https://gist.github.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6
 poke_game = pd.read_csv('pokemoncomplete.csv', index_col=0, encoding= 'unicode_escape')
-poke_go = pd.read_csv('pokemongo.csv', index_col=0, encoding= 'unicode_escape')
+#https://www.kaggle.com/netzuel/pokmon-go-dataset-15-generations
+poke_go = pd.read_csv('pogo.csv', index_col=0, encoding= 'unicode_escape')
 
 # EDA
 poke_go=poke_go.reset_index()
+poke_game.drop(columns=['Type 1','Type 2','Legendary'], inplace=True)
+poke_game.rename(columns={'Attack':'Attack_g','Defense':'Defense_g'}, inplace=True)
 
-poke_go["Total"]=poke_go['Att']+poke_go['Def']+poke_go['Sta']
-poke_game.rename(columns={'Total':'Total_game'},inplace=True)
-#poke_go.info()
+# Inspect NaN
+poke_go.isna().sum()
+poke_game.isna().sum()
 
 poke_go['Name']=poke_go['Name'].str.lower()
 poke_game['Name']=poke_game['Name'].str.lower()
@@ -29,46 +31,97 @@ poke_game['Name']=poke_game['Name'].str.lower()
 # Combine Dataframes
 
 poke_go_compare=poke_go.merge(poke_game,left_on='Name',right_on='Name', how='inner')
+poke_go_compare.drop(columns=['Generation_y'], inplace=True)
+poke_go_compare.rename(columns={'Generation_x':'Generation'}, inplace=True)
 
 # Find Missing Pokemons during Merge
-missing_2=poke_go.loc[poke_go['Dex #'].isin(poke_go_compare['Dex #'])==False,'Name']
+missing=poke_go.loc[poke_go['Pokedex'].isin(poke_go_compare['Pokedex'])==False,'Name']
 
-# Eventually try to add missing pokemons!
+# Eventually try to add missing pokemons!!!
 
 poke_go_complete=poke_go.merge(poke_game,left_on='Name',right_on='Name', how='inner')
-ax=sns.pairplot(poke_go_compare)
-#poke_go_complete.drop(columns=['Type1','Type2'], inplace=True)
+poke_go_complete.drop(columns=['Generation_y'], inplace=True)
+poke_go_complete.rename(columns={'Generation_x':'Generation'}, inplace=True)
 
+# Visualizations
+colInterestgame=['MaxCP','HP','Attack_g','Defense_g','Sp. Atk','Sp. Def','Speed']
+colInterestgo=['MaxCP','Attack','Defense','Stamina']
+
+sns.heatmap(poke_go_complete[colInterestgo].corr(),annot=True)
+sns.heatmap(poke_go_complete[colInterestgame].corr(),annot=True)
+
+sns.pairplot(poke_go_complete.loc[:,colInterestgo])
+
+sns.pairplot(poke_go_complete.loc[:,colInterestgame])
+
+sns.distplot(poke_go_complete.MaxCP).set_title('Distribution of Max CP')
+
+ax=sns.boxplot(x='Primary',y='MaxCP',data=poke_go)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=90)
+
+ax2=sns.boxplot(x='Secondary',y='MaxCP',data=poke_go)
+ax2.set_xticklabels(ax2.get_xticklabels(),rotation=90)
+
+#Legendary Pokemon by Type
+primarycount=poke_go_complete.groupby('Primary').size().sort_values(ascending=False)
+ax3=sns.barplot(x=primarycount.index,y=primarycount)
+ax3.set_xticklabels(ax3.get_xticklabels(),rotation=90)
+
+primarycount=poke_go_complete.groupby('Secondary').size().sort_values(ascending=False)
+ax3=sns.barplot(x=primarycount.index,y=primarycount)
+ax3.set_xticklabels(ax3.get_xticklabels(),rotation=90)
+
+# Modeling using Pokemon Game Data
+
+def rmse(y_pred,y_actual):
+    return np.mean((y_pred-y_actual)**2)
+
+model=m.LinearRegression()
+model.fit(poke_go_complete.loc[:,'HP':'Speed'],poke_go_complete['MaxCP'])
+poke_go_complete['predCP']=model.predict(poke_go_complete.loc[:,'HP':'Speed'])
+
+sns.lmplot(data=poke_go_complete,x='predCP',y='MaxCP')
+
+error=rmse(poke_go_complete['predCP'],poke_go_complete['MaxCP'])
+error
+
+model.coef_
+
+# Model using Pokemon Go Stats
+
+model2=m.LinearRegression()
+model2.fit(poke_go_complete.loc[:,['Attack','Defense','Stamina']],poke_go_complete['MaxCP'])
+poke_go_complete['pokegopred']=model2.predict(poke_go_complete.loc[:,['Attack','Defense','Stamina']])
+
+sns.lmplot(x='pokegopred',y='MaxCP',data=poke_go_complete)
+
+error2=rmse(poke_go_complete['pokegopred'],poke_go_complete['MaxCP'])
+error2
+
+model2.coef_
+
+# Determine Pokemon with Biggest Error
+
+biggestDiff=poke_go_complete.loc[:,['MaxCP','predCP']].diff(axis=1).sort_values('predCP',ascending=False).head(20).index
+
+poke_go_complete.loc[biggestDiff,'Name']
+
+# Predict CP for Gen 6
+
+gen6poke=poke_game.loc[poke_game['Generation']==6,:]
+
+gen6poke['CP']=model.predict(gen6poke.loc[:,'HP':'Speed'])
+
+## Determine strongest Pokemon for Pokemon Games
+# Pokemon GO
+
+#Standardize stats
+def normalization(data):
+    return (data-np.mean(data))/np.std(data)
+poke_go_complete_scaled = pd.DataFrame(normalization(poke_go_complete.loc[:,['Attack','Defense','Stamina']]))
+
+poke_go_complete_scaled['OverallStrength']=np.sum(poke_go_complete_scaled,axis=1)
+poke_go_complete_scaled['Name']=poke_go_complete['Name']
+poke_go_complete_scaled['Primary']=poke_go_complete['Primary']
+poke_go_complete_scaled['Secondary']=poke_go_complete['Secondary']
     
-
-# =============================================================================
-# calclist=['Att','Def','Sta','Total']
-# 
-# for stat in calclist:
-#     slope_stat, intercept_stat, rvalue_stat, pvalue_stat, std_stat = scipy.stats.linregress(poke_go[stat],poke_go['MaxCP'])
-#     r_squared_stat=rvalue_stat**2
-#     ax=sns.lmplot(x=stat, y='MaxCP', data=poke_go)
-#     ax.fig.suptitle(stat+" vs Max CP")
-#     y_pred=slope_stat*poke_go[stat]+intercept_stat
-#     
-#     print("Statistical Values for "+stat)
-#     print("Slope: "+str(round(slope_stat,2)))
-#     print("Intercept: "+str(round(intercept_stat,2)))
-#     print("R^2: "+str(round(r_squared_stat,2)))
-#     print('Mean Absolute Error:', round(metrics.mean_absolute_error(poke_go['MaxCP'], y_pred),2)) 
-#     print('Root Mean Squared Error:', round(np.sqrt(metrics.mean_squared_error(poke_go['MaxCP'], y_pred)),2))
-#     print('\n')
-# 
-# # Visualization
-# 
-# plt.figure(figsize=(15,5))
-# sns.distplot(poke_go.MaxCP).set_title('Distribution of Max CP')
-# 
-# plt.figure(figsize=(15,5))
-# sns.distplot(poke_go.Total).set_title('Distribution of Total')
-# 
-# plt.figure(figsize=(15,5))
-# sns.boxplot(x='Type1',y='MaxCP',data=poke_go)
-# plt.figure(figsize=(15,5))
-# sns.boxplot(x='Type2',y='MaxCP',data=poke_go)
-# =============================================================================
